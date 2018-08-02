aab3e7db39dcde0f92493016ec0b200690b9dd6b
DezkaReid <elmaildeldezkareid@gmail.com>
2018-08-02 11:08:50 -0500

    Added pokemarker to the bundle and register poke-marker element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import PokeMap from './components/PokeMap'
+import PokeMarker from './components/PokeMarker'
 
 window.customElements.define(PokeMap.is, PokeMap)
+window.customElements.define(PokeMarker.is, PokeMarker)
